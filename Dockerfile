c8e948009e1d22f0592f6388909249c45715e334
Fariz <nizaralfariziakbar10@gmail.com>
2025-04-11 16:42:43 +0700

    💩

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# Use official Bun image
+FROM oven/bun:latest
+
+# Set working directory
+WORKDIR /app
+
+# Copy dependencies first for caching
+COPY bun.lock package.json ./
+
+# Install dependencies
+RUN bun install
+
+# Copy the rest of the code
+COPY . .
+
+# Command to run your Bun app (adjust if needed)
+CMD ["bun", "run", "start"]
---
+++ index.ts
@@ -0,0 +1,53 @@
+import axios from "axios";
+
+const ignoreHeaders = [":authority", ":method", ":path", ":scheme"]
+
+async function parseHeadersFromFile(filePath: string): Promise<Record<string, string>> {
+  const rawHeaders = await Bun.file(filePath).text();
+  const headers: Record<string, string> = {};
+
+  const lines = rawHeaders.split("\n")
+
+  if (lines.length % 2 === 0) {
+    let tmpKey: string | null = null
+    lines.forEach(line => {
+      if (tmpKey === null && line.endsWith(":")) {
+        tmpKey = line.slice(0, -1)
+      } else if (tmpKey !== null) {
+        headers[tmpKey] = line;
+        tmpKey = null
+      }
+    });
+  }
+
+  return headers;
+}
+
+async function sendMessage(text: string) {
+  const rawHeaders = await parseHeadersFromFile("headers.txt")
+  const headers = Object.keys(rawHeaders)
+    .filter(key => !ignoreHeaders.includes(key))
+    .reduce<Record<string, any>>((acc, key) => {
+      acc[key] = rawHeaders[key];
+      return acc;
+    }, {});
+
+  const url = `${rawHeaders['origin']}${rawHeaders[':path']}`
+
+  const { data } = await axios.post(url, {
+    "mobile_network_type": "unknown",
+    "content": text,
+    "nonce": String(Math.floor(Date.now() / 1000)),
+    "tts": false,
+    "flags": 0
+  }, {
+    headers
+  })
+
+  // console.log(data)
+}
+
+while (true) {
+  await sendMessage("t!fishy");
+  await Bun.sleep(30 * 1000); // Sleep for 60 seconds
+}
